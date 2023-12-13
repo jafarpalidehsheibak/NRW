44a89c7c602d0fc8e10824955c9dcb19e9a73d9e
jafarpalidehsheibak <jafar.palideh.sheibak@gmail.com>
2023-12-13 21:22:10 +0330

    add first route test

--- routes/api.php
+++ routes/api.php
@@ -15,14 +15,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-Route::group(['prefix' => 'v1'],function(){
-    Route::group(['prefix' => 'auth'], function () {
-        Route::post('/login', [AuthController::class,'login']);
-        Route::post('/logout', [AuthController::class,'logout']);
-        Route::post('/refresh', [AuthController::class,'refresh']);
-        Route::post('/me', [AuthController::class,'me']);
-    });
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+//Route::group(['prefix' => 'v1'],function(){
+//    Route::group(['prefix' => 'auth'], function () {
+//        Route::post('/login', [AuthController::class,'login']);
+//        Route::post('/logout', [AuthController::class,'logout']);
+//        Route::post('/refresh', [AuthController::class,'refresh']);
+//        Route::post('/me', [AuthController::class,'me']);
+//    });
+//});
+
+Route::get('/index',function (){
+    return "hi";
 });
