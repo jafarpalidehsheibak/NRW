3a9fd6e48fa1a2b6010ac379dec9dd9ce4f1d9a8
jafarpalidehsheibak <jafar.palideh.sheibak@gmail.com>
2023-12-14 17:28:43 +0330

    add some user

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -15,8 +15,7 @@ class AuthController extends Controller
     public function login()
     {
         $credentials = request(['email', 'password']);
-
-        if (! $token = auth()->attempt($credentials)) {
+        if (! $token = auth('api')->attempt($credentials)) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
 
@@ -45,7 +44,7 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth('api')->factory()->getTTL() * 60
+            'expires_in' => auth('api')->factory()->getTTL() * 480
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+//    User::create([
+//        'name' => 'sina',
+//        'email' => 'c@c.com',
+//        'password' => \Illuminate\Support\Facades\Hash::make('password'),
+//        'role' => 1
+//    ]);
+    return 'ok';
 });
