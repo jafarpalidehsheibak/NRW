95eeef5bb1ee583f446de185f8b35388005c9933
jafarpalidehsheibak <jafar.palideh.sheibak@gmail.com>
2023-12-20 11:14:13 +0330

    add id to all resource collection

--- app/Http/Controllers/Admin/v1/SupervisorController.php
+++ app/Http/Controllers/Admin/v1/SupervisorController.php
@@ -20,7 +20,7 @@ class SupervisorController extends Controller
 
     public function index()
     {
-        $supervisors = Supervisor::where('role_id', '=', 3)->paginate(5);
+        $supervisors = Supervisor::where('role_id', '=', 3)->paginate(1);
         return response()->json(
             new SupervisorCollection($supervisors)
         , 200);
--- app/Http/Resources/SafetyConsultantResource.php
+++ app/Http/Resources/SafetyConsultantResource.php
@@ -15,6 +15,7 @@ class SafetyConsultantResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id'=>$this->id,
             'name'=>$this->name,
             'username'=>$this->email,
         ];
