af57ec4dcba544daa5b441345f1e062d658aec3a
jafarpalidehsheibak <jafar.palideh.sheibak@gmail.com>
2023-12-21 23:21:40 +0330

    edit supervisor for profile edit

--- app/Http/Controllers/Admin/v1/SupervisorController.php
+++ app/Http/Controllers/Admin/v1/SupervisorController.php
@@ -22,15 +22,15 @@ class SupervisorController extends Controller
     public function index()
     {
 //        $supervisors = Supervisor::where('role_id', '=', 3)->paginate(10);
-        $supervisors =DB::table('users')
-            ->join('profiles','users.id','=','profiles.user_id')
-            ->join('roles','roles.id','=','users.role_id')
-            ->join('provinces','provinces.id','=','profiles.province_id')
-            ->join('experts','experts.id','=','profiles.expert_id')
-            ->where('users.role_id','=',3)->paginate(10);
+        $supervisors = DB::table('users')
+            ->join('profiles', 'users.id', '=', 'profiles.user_id')
+            ->join('roles', 'roles.id', '=', 'users.role_id')
+            ->join('provinces', 'provinces.id', '=', 'profiles.province_id')
+            ->join('experts', 'experts.id', '=', 'profiles.expert_id')
+            ->where('users.role_id', '=', 3)->paginate(10);
         return response()->json(
             new SupervisorCollection($supervisors)
-        , 200);
+            , 200);
     }
 
     public function store(Request $request)
@@ -66,8 +66,7 @@ class SupervisorController extends Controller
                     ],
                 ], 201);
             }
-        }
-        catch (\Exception $e){
+        } catch (\Exception $e) {
             DB::rollBack();
             return response()->json([
                 'data' => [
@@ -79,13 +78,13 @@ class SupervisorController extends Controller
 
     public function show($id)
     {
-        $supervisor =DB::table('users')
-            ->join('profiles','users.id','=','profiles.user_id')
-            ->join('roles','roles.id','=','users.role_id')
-            ->join('provinces','provinces.id','=','profiles.province_id')
-            ->join('experts','experts.id','=','profiles.expert_id')
-            ->where('users.role_id','=',3)
-            ->where('users.id','=',$id)
+        $supervisor = DB::table('users')
+            ->join('profiles', 'users.id', '=', 'profiles.user_id')
+            ->join('roles', 'roles.id', '=', 'users.role_id')
+            ->join('provinces', 'provinces.id', '=', 'profiles.province_id')
+            ->join('experts', 'experts.id', '=', 'profiles.expert_id')
+            ->where('users.role_id', '=', 3)
+            ->where('users.id', '=', $id)
             ->paginate(10);
         if ($supervisor->count() == 0) {
             return response()->json([
@@ -103,32 +102,74 @@ class SupervisorController extends Controller
         $supervisor = Supervisor::query()->where('id', '=', $id)
             ->where('role_id', '=', 3)
             ->get();
-        if ($supervisor->count() == 0) {
+        $profile = Profile::query()->where('user_id', '=', $id)->get();
+        if ($supervisor->count() == 0 || $profile->count() == 0) {
             return response()->json([
                 'message' => 'رکورد مورد نظر یافت نشد'
             ]);
         } else {
             $this->validate($request, [
                 'name' => 'required|string|min:3|max:255',
-                'email' => 'required|digits:11|numeric|regex:/(0)[0-9]{10}/|unique:users,email,' . $id,
                 'password' => 'nullable|string|min:8|max:255',
+                'phone_number' => 'nullable|digits:11|regex:/(0)[0-9]{10}/|numeric|',
+                'expert' => 'nullable|numeric|exists:experts,id',
+                'province' => 'nullable|numeric|exists:provinces,id',
             ]);
             if ($request->has('password')) {
-                $password = Hash::make($request->input('password'));
-                $supervisor->first()->update([
-                    'name' => $request->input('name'),
-                    'email' => $request->input('email'),
-                    'password' => $password,
-                ]);
+                try {
+                    $password = Hash::make($request->input('password'));
+                    DB::beginTransaction();
+                    $supervisor->first()->update([
+                        'name' => $request->input('name'),
+                        'password' => $password,
+                    ]);
+                    $profile->first()->update([
+                        'phone_number' => $request->input('phone_number'),
+                        'expert_id' => $request->input('expert'),
+                        'province_id' => $request->input('province')
+                    ]);
+                    DB::commit();
+                    return response()->json([
+                        'data' => [
+                            'message' => 'رکورد مورد نظر با موفقیت ویرایش شد'
+                        ],
+                    ], 201);
+                } catch (\Exception $e) {
+                    DB::rollBack();
+                    return response()->json([
+                        'data' => [
+                            'message' => 'خطا در ویرایش اطلاعات'
+                        ],
+                    ], 400);
+                }
             } else {
-                $supervisor->first()->update([
-                    'name' => $request->input('name'),
-                    'email' => $request->input('email'),
-                ]);
+                try {
+                    DB::beginTransaction();
+                    $supervisor->first()->update([
+                        'name' => $request->input('name'),
+                    ]);
+                    $profile->first()->update([
+                        'phone_number' => $request->input('phone_number'),
+                        'expert_id' => $request->input('expert'),
+                        'province_id' => $request->input('province')
+                    ]);
+                    DB::commit();
+                    return response()->json([
+                        'data' => [
+                            'message' => 'رکورد مورد نظر با موفقیت ویرایش شد'
+                        ],
+                    ], 201);
+                } catch (\Exception $e) {
+                    DB::rollBack();
+                    return response()->json([
+                        'data' => [
+                            'message' => 'خطا در ویرایش اطلاعات'
+                        ],
+                    ], 400);
+                }
+
             }
-            return response()->json([
-                'data' => 'رکورد مورد نظر با موفقیت ویرایش شد'
-            ], 201);
+
         }
     }
 
@@ -138,16 +179,27 @@ class SupervisorController extends Controller
             ->where('id', '=', $id)
             ->where('role_id', '=', 3)
             ->get();
-//        dd($supervisor);
-        if ($supervisor->count() == 0) {
+        $profile = Profile::query()->where('user_id', '=', $id)->get();
+        if ($supervisor->count() == 0 || $profile->count()==0) {
             return response()->json([
                 'message' => 'رکورد مورد نظر یافت نشد'
             ]);
         } else {
-            $supervisor->first()->delete();
-            return response()->json([
-                'data' => 'رکورد مورد نظر با موفقیت حذف شد'
-            ], 200);
+            try {
+                DB::beginTransaction();
+                $profile->first()->delete();
+                $supervisor->first()->delete();
+                Db::commit();
+                return response()->json([
+                    'data' => 'رکورد مورد نظر با موفقیت حذف شد'
+                ], 200);
+            }
+            catch (\Exception $e){
+                DB::rollBack();
+                return response()->json([
+                    'message' => 'خطا در حذف اطلاعات'
+                ]);
+            }
         }
     }
 }
