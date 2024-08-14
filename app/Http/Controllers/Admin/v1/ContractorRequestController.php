1d692478cb981bcbb14524265a6264328ece0242
jafarpalidehsheibak <jafar.palideh.sheibak@gmail.com>
2024-08-14 15:49:36 +0330

    fix route contract_show_one

--- app/Http/Controllers/Admin/v1/ContractorRequestController.php
+++ app/Http/Controllers/Admin/v1/ContractorRequestController.php
@@ -394,12 +394,23 @@ class ContractorRequestController extends Controller
 
     public function contract_show_one($id)
     {
-
         try {
             $contractor_request_id = Crypt::decrypt($id);
-            $ContractorRequestItem = ContractorRequest::find($contractor_request_id);
+            $res = DB::table('contractor_requests')
+                ->join('provinces','contractor_requests.province_id','=','provinces.id')
+                ->join('cities','contractor_requests.city_id','=','cities.id')
+                ->join('experts','contractor_requests.expert_id','=','experts.id')
+                ->join('status_request','contractor_requests.status','=','status_request.id')
+                ->where('contractor_requests.id','=',$contractor_request_id)
+                ->select('contractor_requests.id','contractor_requests.contractor_name','contractor_requests.contractor_rank',
+                    'contractor_requests.user_id','contractor_requests.road_name','contractor_requests.workshop_location_kilometers','contractor_requests.workshop_begin_lat_long',
+                    'contractor_requests.workshop_end_lat_long','contractor_requests.workshop_name','contractor_requests.full_name_connector','contractor_requests.mobile_connector',
+                    'contractor_requests.email_connector','contractor_requests.approximate_start_date','contractor_requests.workshop_duration','contractor_requests.description',
+                    'contractor_requests.status',
+                    'status_request.status_name',
+                    'provinces.province_name','cities.city_name','experts.name_expert')->first();
             return response()->json(
-                new ContractorRequestResource($ContractorRequestItem)
+                new ContractorRequestResource($res)
                 ,200);
         }
         catch (\Exception $exception){
