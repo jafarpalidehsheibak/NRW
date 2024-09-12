90106e802a276d960f932959061bf4cba71ecf5e
jafarpalidehsheibak <jafar.palideh.sheibak@gmail.com>
2024-09-12 08:29:24 +0330

    fix bug contract show one

--- app/Http/Controllers/Admin/v1/ContractorRequestController.php
+++ app/Http/Controllers/Admin/v1/ContractorRequestController.php
@@ -384,13 +384,16 @@ class ContractorRequestController extends Controller
 
     public function contract_show_one($id)
     {
+//        dd($id);
         try {
             $contractor_request_id = $id;
+//            dd($contractor_request_id);
             $res = DB::table('contractor_requests')
                 ->join('provinces', 'contractor_requests.province_id', '=', 'provinces.id')
                 ->join('cities', 'contractor_requests.city_id', '=', 'cities.id')
                 ->join('experts', 'contractor_requests.expert_id', '=', 'experts.id')
                 ->join('status_request', 'contractor_requests.status', '=', 'status_request.id')
+                ->join('users','users.id','=','contractor_requests.user_id')
                 ->where('contractor_requests.id', '=', $contractor_request_id)
                 ->select('contractor_requests.id', 'contractor_requests.contractor_name', 'contractor_requests.contractor_rank',
                     'contractor_requests.user_id', 'contractor_requests.road_name', 'contractor_requests.workshop_location_kilometers', 'contractor_requests.workshop_begin_lat_long',
@@ -398,7 +401,9 @@ class ContractorRequestController extends Controller
                     'contractor_requests.email_connector', 'contractor_requests.approximate_start_date', 'contractor_requests.workshop_duration', 'contractor_requests.description',
                     'contractor_requests.status',
                     'status_request.status_name',
+                    'users.email',
                     'provinces.province_name', 'cities.city_name', 'experts.name_expert')->first();
+//            dd($res);
             return response()->json(
                 new ContractorRequestResource($res)
                 , 200);
